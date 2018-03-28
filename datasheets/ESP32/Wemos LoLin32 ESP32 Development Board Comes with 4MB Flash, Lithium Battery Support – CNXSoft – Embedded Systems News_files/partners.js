027b58249cf12b009cd784a87f5c1259ca06033a
Paul  Cav <pacav69@gmail.com>
2018-03-28 18:23:36 +1100

    added fire beetle data and spec sheets

---
+++ datasheets/ESP32/Wemos LoLin32 ESP32 Development Board Comes with 4MB Flash, Lithium Battery Support – CNXSoft – Embedded Systems News_files/partners.js
@@ -0,0 +1,86 @@
+window._oiqq = window._oiqq || [];
+_oiqq.push(['oiq_addPageCat','Consumer Electronics']);
+_oiqq.push(['oiq_addPageLifecycle', 'inte']);
+_oiqq.push(['oiq_doTag']);
+
+(function() {
+    var oiq = document.createElement('script'); oiq.type = 'text/javascript'; oiq.async = true;
+    oiq.src = document.location.protocol + '//px.owneriq.net/stas/s/sholic.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(oiq, s);
+})();
+
+
+(function() {
+    var locc_human = document.createElement('script');
+    locc_human.type = 'text/javascript';
+    locc_human.async = true;
+    locc_human.src = document.location.protocol + '//n-cdn.areyouahuman.com/play/YNMJrK4lsMAJlxSsJDb17LW8YmmHRLakZxkWagp6?AYAH_F2=cnx-software.com&AYAH_P2=0a82ae97-af82-457c-b13c-e230cb0238a3&AYAH_F1=Lotame';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(locc_human, s);
+})();
+
+var _comscore = _comscore || [];
+_comscore.push({ c1: "7", c2: "19376307" ,c3: "1" });
+(function() {
+  var s = document.createElement("script"), el = document.getElementsByTagName("script")[0]; s.async = true;
+  s.src = (document.location.protocol == "https:" ? "https://sb" : "http://b") + ".scorecardresearch.com/beacon.js";
+  el.parentNode.insertBefore(s, el);
+})();
+
+(function() {
+  var bkInt = setInterval(function(){
+    if(document.body){
+      clearInterval(bkInt);
+      var bkframe = document.createElement('iframe');
+      bkframe.style.display = 'none';
+      bkframe.src = 'about:blank';
+      bkframe.name = '__bkframe';
+      bkframe.height = 0;
+      bkframe.width = 0;
+      bkframe.frameborder = 0;
+      bkframe.style.position = 'absolute';
+      bkframe.style.clip = 'rect(0px 0px 0px 0px)';
+      document.body.appendChild(bkframe);
+      var bksrc = document.createElement('script');
+      bksrc.src = '//tags.bkrtx.com/js/bk-coretag.js';
+      bksrc.onload = function() {
+                bk_addPageCtx("sh005", "1111745");
+                bk_addPageCtx("sh002", "11560736");
+                bk_addPageCtx("sh005", "1111845");
+                bk_addPageCtx("sh001", "13594596");
+                bk_addPageCtx("sh004", "10813254");
+                bk_addPageCtx("sh004", "10813255");
+                bk_addPageCtx("sh002", "6843806");
+                bk_addPageCtx("sh004", "10813351");
+                bk_addPageCtx("sh004", "10813253");
+                bk_addPageCtx("sh005", "1111763");
+                bk_addPageCtx("sh005", "1111743");
+                bk_addPageCtx("sh005", "1111920");
+                bk_addPageCtx("sh001", "12644393");
+                bk_addPageCtx("sh002", "6322281");
+                bk_addPageCtx("sh004", "10813269");
+                bk_addPageCtx("sh004", "10813248");
+                bk_addPageCtx("sh002", "9233266");
+                bk_addPageCtx("sh001", "10930608");
+                bk_addPageCtx("sh005", "1111750");
+                bk_addPageCtx("sh001", "10930628");
+                bk_addPageCtx("sh001", "13594600");
+                bk_addPageCtx("sh002", "11621944");
+                bk_addPageCtx("sh002", "11621945");
+                bk_addPageCtx("sh004", "10813284");
+                bk_addPageCtx("sh005", "1111754");
+                bk_addPageCtx("sh005", "1111755");
+                bk_addPageCtx("sh005", "6573695");
+                bk_addPageCtx("sh005", "8762417");
+                bk_addPageCtx("sh004", "8762415");
+                bk_doJSTag(41110, 1);
+      };
+      document.body.appendChild(bksrc);
+    }
+  }, 1);
+})();
+(function(){try{var s,w=window.top;w.Tynt=w.Tynt||[];
+w.Tynt.push('sh!sh');s=w.document.createElement('script');
+s.src='https://cdn.tynt.com/afsh.js';
+(w.document.getElementsByTagName('head')[0]).appendChild(s);}catch(e){}})();
+
